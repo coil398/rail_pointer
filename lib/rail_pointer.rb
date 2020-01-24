d1af1fa8ab2469b42d196220bdf3b201303bd72d
takumi-kawase <konayuta.sou@gmail.com>
2020-01-24 17:04:00 +0900

    created gem

---
+++ lib/rail_pointer.rb
@@ -0,0 +1,6 @@
+require "rail_pointer/version"
+
+module RailPointer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rail_pointer_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RailPointer do
+  it "has a version number" do
+    expect(RailPointer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
